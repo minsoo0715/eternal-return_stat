705b3a92674ee4fdb30b959ba6ca614c9daa0a10
WhiteB <whiteblock0715@gmail.com>
2021-06-22 08:44:21 +0900

    init

---
+++ src/test/java/dimigo/EternalStat/MemberControlApplicationTests.java
@@ -0,0 +1,13 @@
+package dimigo.EternalStat;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MemberControlApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
